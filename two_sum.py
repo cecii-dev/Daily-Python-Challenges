02743afd3e09e31b855a88c7601613573ea266fd
cecii-dev <cecibgut@gmail.com>
2026-05-08 18:56:15 +0100

    feat: description and skeleton for Two Sum challenge

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+"""
+LeetCode #1: Two Sum
+Description: Find two distinct indices in a list where the sum of their values 
+equals a predefined target.
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
