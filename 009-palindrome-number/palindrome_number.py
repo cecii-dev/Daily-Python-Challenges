94f3dfd4afd849998f728dafeb3b2cfdb0689cfb
cecii-dev <cecibgut@gmail.com>
2026-05-09 17:57:35 +0100

    feat: add description and skeleton for Palindrome Number challenge

---
+++ 009-palindrome-number/palindrome_number.py
@@ -0,0 +1,9 @@
+"""
+LeetCode #9: Palindrome Number
+Description: Determine whether an integer is a palindrome. 
+An integer is a palindrome when it reads the same backward as forward.
+"""
+
+class Solution(object):
+    def isPalindrome(self, x:int):
+        pass
