1089cf9f22998b9b0bf5d50e720441f3226689ef
cecii-dev <cecibgut@gmail.com>
2026-05-09 18:32:51 +0100

    feat: add logic to reverse integer as string and compare

--- 009-palindrome-number/palindrome_number.py
+++ 009-palindrome-number/palindrome_number.py
@@ -6,4 +6,13 @@ An integer is a palindrome when it reads the same backward as forward.
 
 class Solution(object):
     def isPalindrome(self, x:int):
-        pass
+
+        if x < 0: # Negative number can't be a palindrome
+            return False
+        else:
+            x = str(x)
+            result = x[::-1]
+            if result == x:
+                return True
+            else:
+                return False
